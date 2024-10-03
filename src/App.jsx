ba3d6f378dca4d67a8135c5bd7b48f7a956a5dfa
Santosh Pujari <santosh.pujari@hotmail.com>
2024-10-03 11:31:51 -0500

    Added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
@@ -6,8 +6,13 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [todoValue, setTodoValue] = useState('');
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }));
+  }
+
   function handleAddTodos(newTodo) {
     const newToDos = [...todos, newTodo];
+    persistData(newToDos);
     setTodos(newToDos);
   }
 
@@ -24,6 +29,19 @@ function App() {
     setTodoValue(valueTobeEdit);
     handleDeleteToDo(index);
   }
+
+  useEffect(() => {
+    if (!localStorage) {
+      return;
+    }
+
+    let localToDos = localStorage.getItem('todos');
+    if (!localToDos) {
+      return;
+    }
+    localToDos = JSON.parse(localToDos).todos;
+    setTodos(localToDos);
+  }, []);
 
   return (
     <>
